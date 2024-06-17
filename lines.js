b4101b3ffef3d706384a62da0b193827ceffe6f1
emre g. <emreodev1998@gmail.com>
2024-06-17 22:19:20 +0300

    convert it to modules

---
+++ lines.js
@@ -0,0 +1,48 @@
+
+const rightShape={
+
+    load(){
+
+        this.canvasContainer=document.querySelector(".nonstatic");
+        this.canvas=document.querySelector(".nonstatic > canvas");
+    },
+  
+    drawLines(number){
+
+   const ctx = this.canvas.getContext("2d");
+   ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+   ctx.save();
+
+   ctx.lineWidth = 2;
+   ctx.strokeStyle="#6a040f";
+
+   ctx.beginPath();
+   const bottomMiddlePoint={x:this.canvas.width/2, y:this.canvas.height};
+   const space_x=this.canvas.width/(number-1);
+   for(let i=0;i<number;i++){
+
+       ctx.moveTo(i*space_x,0);
+       ctx.lineTo(bottomMiddlePoint.x, bottomMiddlePoint.y);
+   }
+
+   ctx.stroke();
+   ctx.restore();
+
+    },
+    resetCanvasSize(bottomtext){
+
+       this.canvas.width=this.canvasContainer.offsetWidth;
+       this.canvas.height=bottomtext.getBoundingClientRect().bottom+window.scrollY -100;
+
+    }
+
+}
+
+
+function makeShinyLine(){
+
+
+
+}
+
+export {rightShape,makeShinyLine}
--- script.js
+++ script.js
@@ -1,49 +1,16 @@
 
-const rightShape={
-
-     canvasContainer:document.querySelector(".nonstatic"),
-     canvas:document.querySelector(".nonstatic > canvas"),
-     drawLines(number){
-
-    const ctx = this.canvas.getContext("2d");
-
-    ctx.save();
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle="#6a040f";
-
-    ctx.beginPath();
-    const bottomMiddlePoint={x:this.canvas.width/2, y:this.canvas.height};
-    const space_x=this.canvas.width/(number-1);
-    for(let i=0;i<number;i++){
-
-        ctx.moveTo(i*space_x,0);
-        ctx.lineTo(bottomMiddlePoint.x, bottomMiddlePoint.y);
-    }
-
-    ctx.stroke();
-    ctx.restore();
-
-     },
-     resetCanvasSize(){
-
-        const bottomtext=document.querySelector(".bottom-text-sec");
-
-        this.canvas.width=this.canvasContainer.offsetWidth;
-        this.canvas.height=bottomtext.getBoundingClientRect().bottom+window.scrollY -100;
-
-     }
-
-}
+import { rightShape,makeShinyLine } from "./lines.js";
     
+rightShape.load();
 
 window.addEventListener("load",()=>{
-    rightShape.resetCanvasSize();
+    rightShape.resetCanvasSize(document.querySelector(".bottom-text-sec"));
     rightShape.drawLines(15);
 })
 
 window.addEventListener("resize",()=>{
-    rightShape.resetCanvasSize();
+    rightShape.resetCanvasSize(document.querySelector(".bottom-text-sec"));
+    rightShape.drawLines(15);
 })
 
 
